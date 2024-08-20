f55dfd32ae2a8a19f63786e08abcb8c53256c826
Developer Nur <nursweb6@gmail.com>
2024-08-20 22:03:07 +0600

    Banner section added with responsive layout

--- smart-phone-client/src/Pages/Home/Home.jsx
+++ smart-phone-client/src/Pages/Home/Home.jsx
@@ -1,8 +1,25 @@
+import BannerImage from "/images/bannerImage.jpg"
 
 const Home = () => {
     return (
         <>
-           <h2 className="text-2xl">Smart Phone Home page</h2> 
+            <div className=" hero lg:min-h-screen flex flex-col lg:flex-row justify-between items-center gap-5 px-5 md:px-10 lg:px-20 my-10 bg-gray-100 rounded-lg">
+                <div className="w-full lg:w-2/4">
+                    <h1 className="text-5xl font-bold">Get a new smart phone</h1>
+                    <p className="py-6">
+                        Acquisition of a new smartphone entails embracing cutting-edge technology and enhanced connectivity. Seek to explore innovative features and improved performance that redefine user experience.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+                <div className="w-full lg:w-2/4">
+                    <img
+                        src={BannerImage}
+                        className="w-full mx-auto rounded-lg shadow-2xl" />
+                </div>
+
+            </div>
+
+
         </>
     );
 };
